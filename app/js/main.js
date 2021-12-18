be90eabc8aeac6af8e4cfea6b603aff4dc7c9d6b
DimaKosyk <dimakosyk93@gmail.com>
2021-12-18 02:23:39 +0200

    added product-list1 and product-list2

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,20 @@
 $(function(){
+
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+  });
+
+  $('.select-style').styler();
+
   $('.top-slider__items').slick({
     arrows: false,
     autoplay: true,
@@ -15,6 +31,31 @@ $(function(){
   $('.design__filter-btn').on('click', function () {
     $('.design__filter-btn').removeClass('design__filter-btn--active');
     $(this).addClass('design__filter-btn--active');
+  });
+
+  $('.cart-btn').on('click', function () {
+    $('.cart-btn').removeClass('cart-btn--active');
+    $('.product-list__add-btn').addClass('cart-btn--active');
+  });
+
+  $('.product-list__add-btn').on('click', function () {
+    $('.product-list__add-btn').removeClass('cart-btn--active');
+    $('.cart-btn').addClass('cart-btn--active');
+  });
+
+  $(".star").rateYo({
+    starWidth: "11px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true
+  });
+
+  $(".star-line").rateYo({
+    starWidth: "18px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    spacing: "13px",
+    readOnly: true
   });
 
   var containerEl1 = document.querySelector('[data-ref="container-1"]');
