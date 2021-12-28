3b6eb6261ab6e6b200418360039937826ca511b5
DimaKosyk <dimakosyk93@gmail.com>
2021-12-29 00:29:50 +0200

    added moduls, adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,9 @@
 $(function(){
 
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').toggleClass('menu__list--active');
+  });
+
   $('.product-detail__top-item').on('click', function(e){
     e.preventDefault();
     $('.product-detail__top-item').removeClass('product-detail__top-item--active');
